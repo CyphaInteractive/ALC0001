b74f386b333fc65bca971dc8b21722fadc622aec
DeeCypha <daniel@wearecypha.com.au>
2013-09-18 16:01:19 +1000

    First commit

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Larykan</title>
+    <link rel="stylesheet" type="text/css" href="css/final.css">
+    <!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+<body>
+    <div id="fb-root"></div>
+    <script src="https://connect.facebook.net/en_US/all.js"></script>
+    <script>
+        FB.init({
+            appId: '634820256538952',
+            cookie: true
+        });
+        FB.Canvas.setAutoGrow();
+    </script>
+    <div id="wrapper-1">
+        <div id="wrapper-2">
+            <header>
+                <h1>
+                    <img src="images/larykan.jpg" class="img-1" alt="LARYKAN"></h1>
+
+                <div class="video-wrapper">
+                    <!--<img src="images/video.jpg" class="img-2" alt="">-->
+                    <iframe width="749" height="402" src="//www.youtube.com/embed/7qko_J6f8rw?rel=0" frameborder="0" allowfullscreen></iframe>
+                </div>
+            </header>
+            <div id="secondary">
+                <!--			<div class="div-01">
+				<a class="a-1 current" href="http://www.yahoo.com">1</a>
+				<a class="a-2" href="http://www.yahoo.com">2</a>
+				<a class="a-3" href="http://www.yahoo.com">3</a>
+				<a class="a-4" href="http://www.yahoo.com">4</a>
+				<a class="a-5" href="http://www.yahoo.com">5</a>
+			</div>-->
+                <h2>Show Info</h2>
+                <aside class="aside-1">
+                    <h3>MONA VALE HOTEL</h3>
+                    <time>20 Sept 2013, 8pm</time>
+                    <p>Alchemy Music Group, Premier Artists and Kingdom Sounds Present: LARYKAN w/ Vegas Aces, Baskiat, Nonne MC & Jason Matts. Tickets on sale from Oztix</p>
+                    <a class="a-2" href="http://tickets.oztix.com.au/default.aspx?Event=37906" target="_blank">BUY TICKETS</a>
+                </aside>
+                <aside class="aside-2">
+                    <h3>OXFORD ART FACTORY</h3>
+                    <time>26 Sept 2013, 8pm</time>
+                    <p>THE HAVKNOTZ + LARYKAN + NHOSTIC + DECYPHER US + THE FREEDOM FIGHTERS</p>
+                    <a class="a-3" href="http://www.moshtix.com.au/event.aspx?id=68021&skin=" target="_blank">BUY TICKETS</a>
+                </aside>
+                <!--				<aside class="aside-3">
+					<h3>ANNANDALE HOTE</h3>
+					<time>22 Feb 2014, 7pm</time>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+					sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+					Ut enim ad minim </p>
+					<a class="a-4" href="http://tickets.oztix.com.au/default.aspx?Event=37906">BUY TICKETS</a>
+				</aside>
+				<aside class="aside-4">
+					<h3>FACTORY THEATRE</h3>
+					<time>22 Feb 2014, 7pm</time>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+					sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+					Ut enim ad minim veniam, quis nostrud </p>
+					<a class="a-5" href="http://www.yahoo.com">BUY TICKETS</a> 
+				</aside>-->
+                <br style="clear: both">
+            </div>
+            <!-- secondary -->
+            <div id="primary">
+                <!--		<div class="div-01">
+			<a class="a-1 current" href="http://www.google.com">1</a>
+			<a class="a-2" href="http://www.google.com">2</a>
+			<a class="a-3" href="http://www.google.com">3</a>
+			<a class="a-4" href="http://www.google.com">4</a>
+			<a class="a-5" href="http://www.google.com">5</a>
+		</div>	-->
+                <h2>Larykan Info</h2>
+                <article class="article-1">
+                    <h3>WHO IS LARYKAN</h3>
+                    <p>One thing about speaking your truth is that regardless of whether the listener agrees with it, they are compelled to respect your view point, being transparent and speaking from the heart is sometimes very confronting, but it is The Truth! This is LARYKAN.</p>
+                    <p>LARYKAN is a powerhouse of all things “real” and spits it with passion, after having spent time finding his voice through many collaborations and experimentation, his TRUTH is now realised</p>
+                    <!--<a class="a-6" href="http://www.google.com">READ MORE</a>-->
+                </article>
+                <article class="article-2">
+                    <h3>WHO IS LARYKAN</h3>
+                    <p>All things being “perfect and as it should be”, LARYKAN has manifested his reality by teaming up with with his Producer Sammy Kannis, the flow is on and his Mantra is “Transmute, Transform, Transcend, Translate, Love and Light Mother F*#ker, NO HATE!</p>
+                    <p>
+                        LARYKAN is not here to revolutionise or change the world or the Hip Hop scene, he is here to shed some LIGHT on it. 
+                        Contact: info@alchemymusicgroup.com.au
+                    </p>
+                    <!--<a class="a-7" href="http://www.google.com">READ MORE</a>-->
+                </article>
+                <br style="clear: both">
+            </div>
+            <div class="image-wrapper">
+                <!--<img class="img-5" src="images/img-5.jpg" alt="">-->
+                <iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F13030707"></iframe>
+                <iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F9162456"></iframe>
+
+            </div>
+            <footer>
+                <img class="img-6" src="images/img-6.jpg" alt="">
+                <img class="img-7" src="images/img-7.jpg" alt="">
+                <!--<img class="img-8" src="images/img-8.jpg" alt="">-->
+                <br style="clear: both" />
+            </footer>
+        </div>
+    </div>
+</body>
+</html>
